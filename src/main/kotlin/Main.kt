1924391743f85fa301ccc6b74fe3f717e807c3b6
Jakub Dąbek <jakub.dabek@gmail.com>
2021-05-26 19:00:19 +0200

    Add simple main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
+import com.github.javaparser.utils.ParserCollectionStrategy
+import java.nio.file.Path
+
 fun main(args: Array<String>) {
-    println("Hello, $args")
+    println("Hello, ${args.joinToString(prefix = "[", postfix = "]")}")
+
+    if (args.isEmpty())
+        return
+
+    val projectRoot = ParserCollectionStrategy().collect(Path.of(args[0]))
+    println(projectRoot.sourceRoots.joinToString("\n"))
+    println()
+
+    val pom = projectRoot.root.resolve("pom.xml")
+    val project = PomProject(pom)
+    fun printModuleTests(module: PomProject) {
+        println("${module.name}: ${module.allTests?.joinToString("\n\t")}")
+    }
+    printModuleTests(project)
+    for (module in project.recursiveModules) {
+        printModuleTests(module)
+    }
 }
